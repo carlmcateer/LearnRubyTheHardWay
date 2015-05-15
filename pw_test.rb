61c41720019a2067bd9bc2ad14d5c87edf5a2bb9
CarlMcAteer <carlmc12@gmail.com>
2015-05-15 16:23:55 +0100

    Small formating change to pw_test.rb

--- pw_test.rb
+++ pw_test.rb
@@ -1,27 +1,17 @@
 puts "Type a file name please"
 file = $stdin.gets.chomp
-
-password = "carlisgreat"
-
+password = "1234567890"
 print "Password: "
 user_pw = $stdin.gets.chomp
 if password == user_pw
-
   puts open(file).read
-
 else
-
   puts "Nope! One more time bucko!"
   print "Password: "
-  attmpt_2 = $stdin.gets.chomp
-
-  if password == attmpt_2
-
+  second_try = $stdin.gets.chomp
+  if password == second_try
     puts open(file).read
-
   else
-
     puts "Not this time!"
-
   end
 end
